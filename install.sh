ed3e94e6420868799fcfacf7b3bf5ea7389ae6b8
ada <ada.okanu@sfr.fr>
2025-02-27 13:10:02 +0100

    Ajout script Net Beans

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# installation de netbeans 
+
+#Mise a jour de la base des packets 
+sudo apt update -y 
+
+#Aller dans le dossier opt
+cd /opt 
+
+#recuperer netbeans sur le site officiel 
+sudo wget https://dlcdn.apache.org/netbeans/netbeans-installers/25/apache-netbeans_25-1_all.deb
+ 
+sudo apt install ./apache-netbeans_25-1_all.deb 
+
