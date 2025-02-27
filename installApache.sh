6bcab546daae72eead54968622ee0de2797984d9
ada <ada.okanu@sfr.fr>
2025-02-27 14:20:40 +0100

    Ajout de lignes sur script Apache

--- installApache.sh
+++ installApache.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#Mise à jour système
 sudo apt install apache2
-
+#Installation
 sudo service apache2 start
+#Démarage 
+sudo systemctl start apache2
+#Vérifier le statuts 
+sudo systemctl status apache2
