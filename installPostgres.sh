0c066d8abf5db36ede5b1295c3f57b5da7c604fb
ada <ada.okanu@sfr.fr>
2025-02-27 15:36:08 +0100

    une modification du script Postgres

--- installPostgres.sh
+++ installPostgres.sh
@@ -3,5 +3,5 @@
 # Mise à jours 
 sudo apt update
 #le moteur de base de données 
-apt install postgresql postgresql-client postgresql-contrib
+sudo apt install postgresql postgresql-client postgresql-contrib
 
