884aab2aaacc0f17f0ea2328dc1278c530692545
ada <ada.okanu@sfr.fr>
2025-03-06 13:58:43 +0100

    Ajout de commandes dans script postgresql

--- installPostgres.sh
+++ installPostgres.sh
@@ -4,4 +4,7 @@
 sudo apt update
 #le moteur de base de données 
 sudo apt install postgresql postgresql-client postgresql-contrib
-
+# pour connaitre le status 
+systemctl status postgresql
+# Si le service n’est pas démarré ni activé au démarrage
+ systemctl enable --now postgresql
