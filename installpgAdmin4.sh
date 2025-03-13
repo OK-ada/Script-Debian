4472debaed3fe5ea9e539fd20745060d1bddda9b
ada <ada.okanu@sfr.fr>
2025-03-13 15:31:55 +0100

    Modification du script PgAdmin

--- installpgAdmin4.sh
+++ installpgAdmin4.sh
@@ -1,7 +1,5 @@
-#
+#!/bin/bash
 # Setup the repository
-#
-
 # Install the public key for the repository (if not done previously):
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 
@@ -10,12 +8,13 @@ sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] h
 
 #
 # Install pgAdmin
+#
 
 # Install for both desktop and web modes:
 sudo apt install pgadmin4
 
 # Install for desktop mode only:
-sudo apt install pgadmin4-Bureau
+sudo apt install pgadmin4-desktop
 
 # Install for web mode only: 
 sudo apt install pgadmin4-web 
