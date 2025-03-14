5f633ba6ba1602b7e09cf20855e4e79bb17b318a
ada <ada.okanu@sfr.fr>
2025-03-14 10:22:59 +0100

    Modification de Tomcat9

--- installTomcat.sh
+++ installTomcat.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-
 # Mettre à jour les paquets
 sudo apt update
 #Installation de Java  
 sudo apt install default-jdk
 # Voir la version de Java
 java -version
-#Création de l'user 
-useradd -r -d /opt/tomcat9 -s /bin/false tomcat
+#Création répertoir pour ajouter un user 
 mkdir /opt/tomcat9
 # Téléchargement et installation de Tomcat 9 rechercher dans les archives 
 VER=9.0.102
@@ -17,23 +14,6 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.t
 #Extrayez le binaire Apache Tomcat 9 dans le répertoire /opt/tomcat9 .
 tar xzf apache-tomcat-${VER}.tar.gz -C /opt/tomcat9 --strip-components=1
 
-#Confirmez le contenu du répertoire personnel d’Apache Tomcat 
-ls -alh1 /opt/tomcat9/
-#Assurez-vous que l'utilisateur Tomcat possède les répertoires, work , temp , webapps et logs
-chown -R tomcat: /opt/tomcat9/{logs,temp,webapps,work}
-chown -R :tomcat /opt/tomcat9/
-# autorisation 
-chmod -R g+r /opt/tomcat9/conf
-chmod g+x /opt/tomcat9/conf
-# Configuration 
-echo 'export CATALINA_HOME="/opt/tomcat9"' > /etc/profile.d/tomcat9.sh
-
-update-java-alternatives -l
-java-1.17.0-openjdk-amd64      1711       /usr/lib/jvm/java-1.17.0-openjdk-amd64
-echo 'export JAVA_HOME=" /usr/lib/jvm/java-1.17.0-openjdk-amd64 "' >> /etc/profile.d/tomcat9.sh
-source /etc/profile.d/tomcat9.sh
-#lancer Tomcat9
-opt/tomcat9/bin/startup.sh
 # Afficher le message de réussite
 echo "Accédez à l'interface d'administration sur : http://localhost:8080 (nécessite configuration des utilisateurs)"
 # Activer le service 
