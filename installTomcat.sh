9fbd7a49c1b03329140b3f4f956f34a9084d300c
ada <ada.okanu@sfr.fr>
2025-03-14 09:39:39 +0100

    Modification de Tomcat9

--- installTomcat.sh
+++ installTomcat.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-# Vérifier si le script est exécuté en tant que root
-if [ "$EUID" -ne 0 ]; then
-  echo "Veuillez exécuter ce script avec sudo: sudo ./installTomcat.sh"
-  exit 1
-fi
 
 # Mettre à jour les paquets
 sudo apt update
+#Installation de Java  
+sudo apt install default-jdk
+# Voir la version de Java
+java -version
+#Création de l'user 
+useradd -r -d /opt/tomcat9 -s /bin/false tomcat
+mkdir /opt/tomcat9
+# Téléchargement et installation de Tomcat 9 rechercher dans les archives 
+VER=9.0.102
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.tar.gz
 
-# Installer Tomcat 9 et les outils d'administration
-sudo apt install -y tomcat9 tomcat9-admin
+#Extrayez le binaire Apache Tomcat 9 dans le répertoire /opt/tomcat9 .
+tar xzf apache-tomcat-${VER}.tar.gz -C /opt/tomcat9 --strip-components=1
 
-# Activer et démarrer Tomcat
-systemctl enable tomcat9
-systemctl start tomcat9
+#Confirmez le contenu du répertoire personnel d’Apache Tomcat 
+ls -alh1 /opt/tomcat9/
+#Assurez-vous que l'utilisateur Tomcat possède les répertoires, work , temp , webapps et logs
+chown -R tomcat: /opt/tomcat9/{logs,temp,webapps,work}
+chown -R :tomcat /opt/tomcat9/
+# autorisation 
+chmod -R g+r /opt/tomcat9/conf
+chmod g+x /opt/tomcat9/conf
+# Configuration 
+echo 'export CATALINA_HOME="/opt/tomcat9"' > /etc/profile.d/tomcat9.sh
 
-# Vérifier le statut de Tomcat
-systemctl status tomcat9 --no-pager
-
+update-java-alternatives -l
+java-1.17.0-openjdk-amd64      1711       /usr/lib/jvm/java-1.17.0-openjdk-amd64
+echo 'export JAVA_HOME=" /usr/lib/jvm/java-1.17.0-openjdk-amd64 "' >> /etc/profile.d/tomcat9.sh
+source /etc/profile.d/tomcat9.sh
+#lancer Tomcat9
+opt/tomcat9/bin/startup.sh
 # Afficher le message de réussite
-echo "Tomcat 9 et Tomcat 9 Admin ont été installés avec succès !"
-echo "Accédez à l'interface d'administration sur : http://localhost:8080/manager/html (nécessite configuration des utilisateurs)"
+echo "Accédez à l'interface d'administration sur : http://localhost:8080 (nécessite configuration des utilisateurs)"
 
 # Execution 
 #chmod +x installTomcat.sh
